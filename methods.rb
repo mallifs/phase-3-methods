a08759e42ab5ea91d67f46924b3f641b2d0e9aa6
Leon simintei <mallfs@icloud.com>
2023-02-07 03:54:04 +0300

    assignment complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
-# Your code here!
+def greet_programmer
+    puts 'Hello, programmer!'
+end
+
+def greet(name = 'Naureen')
+    puts "Hello, #{name}!"
+end
+greet 'Jimmy'
+
+def greet_with_default (name = 'programmer')
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+     num1 + num2
+end
+add(2,2)
+
+def halve(num)
+    return nil unless num.is_a? Integer
+    num / 2
+  end
